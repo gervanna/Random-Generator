9b0f741bf08a8e4a0f3634c098c6aca941b671a8
gervanna <stephenslavern@gmail.com>
2022-03-26 17:37:01 -0600

    added twitter share for fun facts

--- script.js
+++ script.js
@@ -3,15 +3,24 @@ const randomFunFact = document.getElementById("randomFunFact");
 const dogImage = document.getElementById("dogImage");
 const randomDog = document.getElementById("randomDog");
 const dogText = document.getElementById("dogText");
+const tweetBtn = document.getElementById("tweet");
+// const tweetBtn2 = document.getElementById("tweet2");
 
 
 function getFunFact() {
+    const randomGenUrl = "https://gervanna.github.io/random-generator/";
+
+
     fetch("https://api.aakhilv.me/fun/facts")
     .then(response => response.json())
     .then(data => {
-        funFactText.textContent = data;
+        display = funFactText.textContent = data;
     })
     .catch(err=>console.log(err));
+    tweetBtn.addEventListener('click', () => {
+        tweetBtn.href = `https://twitter.com/intent/tweet?text="${display}"  —courtesy of theFunFactAPI via &url=${randomGenUrl}`;
+    })
+    
 };
 
 function getDog() {
@@ -21,7 +30,10 @@ function getDog() {
         dogImage.src = result.message
     })
     .catch(err=>console.log(err));
+    // tweetBtn2.addEventListener('click', () => {
+    //     tweetBtn2.href = "https://twitter.com/intent/tweet?url="+result.message;
+    // })
 };
 
 randomFunFact.addEventListener('click', getFunFact);
-randomDog.addEventListener('click', getDog); 
+randomDog.addEventListener('click', getDog);
